55c711bef27b8a7dad6fa85377c0303310216508
Joseph Sabido <joesabido@gmail.com>
2018-09-27 21:36:58 -0700

    Consolidate MW and add shell

--- src/index.js
+++ src/index.js
@@ -5,40 +5,28 @@ import Path from 'path'
 import Fs from 'fs'
 import Http from 'http'
 import Cors from 'cors'
-//import Readline from 'readline'
 
-import SmoothieboardMw from './lib/SmoothieboardMw'
+import Smoothieboard from './lib/Smoothieboard'
 
 const app = Express()
 const server = Http.createServer(app)
 const serverPort = 3000
 const swaggerConfig = JsYaml.safeLoad(Fs.readFileSync(Path.join(__dirname, '/swagger.yaml'), 'utf8'))
-
-/*
-const ReadlinePrompt = Readline.createInterface({
-	input : process.stdin,
-	output : process.stdout,
-	terminal : false,
-	prompt : 'Command > '
+const swaggerControllers = Path.join(__dirname, '/controllers')
+const smoothie = new Smoothieboard({
+	server : server,
+	portDevice : '/dev/ttyACM0'
 })
-
-ReadlinePrompt.on('line', (line) => {
-	if(line !== undefined && line.toString().trim() !== ''){
-		//SmoothieboardInterface.sendCommand(line.toString().trim().toUpperCase())
-		//ReadlinePrompt.prompt()
-	}
-})
-*/
 
 SwaggerTools.initializeMiddleware(swaggerConfig, swaggerMw => {
 	app.use(Cors())
-	app.use(SmoothieboardMw(server))
+	app.use(smoothie.middleware())
 	app.use(swaggerMw.swaggerMetadata())
 	app.use(swaggerMw.swaggerValidator())
-	app.use(swaggerMw.swaggerRouter({ controllers : Path.join(__dirname, '/controllers') }))
+	app.use(swaggerMw.swaggerRouter({ controllers : swaggerControllers }))
 	app.use(swaggerMw.swaggerUi())
 	
 	server.listen(serverPort, () => {
-		//ReadlinePrompt.prompt()
+		smoothie.showShell()
 	})
 })
--- src/lib/Smoothieboard.js
+++ src/lib/Smoothieboard.js
@@ -3,6 +3,7 @@ import Binding from '@serialport/bindings'
 import BindingMock from '@serialport/binding-mock'
 import Fs from 'fs'
 import SocketIO from 'socket.io'
+import Readline from 'readline'
 
 class Smoothieboard{
 	constructor(options){		
@@ -11,15 +12,32 @@ class Smoothieboard{
 		this.port = null
 		this.mockPort = false
 		this.socket = null
-		
+		this.shell = null
 		this.initialize()
-		this.openPort()
 	}
 
 	initialize = () => {
 		this.socket = SocketIO(this.server)
+		this.openPort()
+		this.createShell()
 	}
+	
+	createShell = () => {
+		this.shell = Readline.createInterface({
+			input : process.stdin,
+			output : process.stdout,
+			terminal : false,
+			prompt : 'Command > '
+		})
 
+		this.shell.on('line', line => {
+			if(line !== undefined && line.toString().trim() !== ''){
+				this.sendCommand(line.toString().trim().toUpperCase())
+				this.showShell()
+			}
+		})
+	}
+	
 	openPort = () => {
 		if(!this.portDevice || this.portDevice === undefined || this.portDevice.trim() === ''){
 			this.socket.emit('error', 'openPort requires portDevice.')
@@ -59,7 +77,19 @@ class Smoothieboard{
 	}
 
 	sendCommand = (command) => {
+		this.socket.emit('data', `Command being sent: ${command}`)
 		this.port.write(`${command}\n`)
+	}
+
+	middleware = () => {
+		return (request, response, next) => {
+			request.smoothie = this
+			next()
+		}
+	}
+
+	showShell = () => {
+		this.shell.prompt()
 	}
 }
 
